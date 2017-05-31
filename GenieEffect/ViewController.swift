ca07dd03d578ac8bf5845d63a1f0c20b82aab9c5
Brad Gayman <bgayman@apppartner.com>
2017-05-31 10:38:26 -0400

    Initial Commit

---
+++ GenieEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenieEffect
+//
+//  Created by App Partner on 5/31/17.
+//  Copyright © 2017 App Partner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
